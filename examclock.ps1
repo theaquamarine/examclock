dbac81633c49f2b82563e9e802fe85d7cb922234
blue <playbynumbers@gmail.com>
2019-02-24 14:32:01 +0000

    Add date and relevant JCQ rules

--- examclock.ps1
+++ examclock.ps1
@@ -1,12 +1,25 @@
-﻿Add-Type -AssemblyName PresentationFramework
+﻿# JCQ Instructions for Conduction Examinations, Section 11
+# 11.7 A reliable clock (analogue or digital) must be visible to each candidate in the examination room.
+# The clock must be big enough for all candidates to read clearly.
+# The clock must show the actual time at which the examination starts.
+# Countdown and ‘count up’ clocks are not permissible.
+
+# 11.9 A board/flipchart/whiteboard should be visible to all candidates showing the:
+#     a) centre number, subject title and paper number; and
+#     b) the actual starting and finishing times, and date, of each examination.
+
+
+Add-Type -AssemblyName PresentationFramework
 
 [xml]$xaml = Get-Content -Path $PSScriptRoot\examclock.xaml
 $window = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
 
 $clock = $window.FindName('Clock')
+$date = $window.FindName('Date')
 
 function Update-Time {
-    $clock.Text = "$(Get-Date -format 'h:mm')" # 12-hour clock without seconds probably preferable
+    $clock.Text = "$(Get-Date -Format t)" # 12-hour clock without seconds probably preferable
+    $date.Text = "$(Get-Date -Format d)"
 }
 
 $window.Add_SourceInitialized({

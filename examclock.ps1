0ed7cd6856faf39505042d67efab4fdadc9ae654
blue <playbynumbers@gmail.com>
2019-02-24 13:21:37 +0000

    A clock!

--- examclock.ps1
+++ examclock.ps1
@@ -2,6 +2,20 @@
 $window = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
 
 $clock = $window.FindName('Clock')
-$clock.Text = "$(Get-Date -format 'h:mm')" # 12-hour clock without seconds probably preferable
+
+$updateTime = {
+    $clock.Text = "$(Get-Date -format 'h:mm')" # 12-hour clock without seconds probably preferable
+}
+
+$window.Add_SourceInitialized({
+    $timer = New-Object System.Windows.Threading.DispatcherTimer
+    $timer.Interval = New-TimeSpan -Seconds 1
+    $timer.Add_Tick({
+        $updateTime.Invoke()
+    })
+    $timer.Start()
+})
+
+$updateTime.Invoke()
 
 $window.ShowDialog()

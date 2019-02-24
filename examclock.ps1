6313a851640f43db4a7ffdb6a7b29b43e92c7360
blue <playbynumbers@gmail.com>
2019-02-24 13:18:53 +0000

    A window with the current time

--- examclock.ps1
+++ examclock.ps1
@@ -1,3 +1,7 @@
 ﻿[xml]$xaml = Get-Content -Path $PSScriptRoot\examclock.xaml
 $window = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+
+$clock = $window.FindName('Clock')
+$clock.Text = "$(Get-Date -format 'h:mm')" # 12-hour clock without seconds probably preferable
+
 $window.ShowDialog()
